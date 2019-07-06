b04367b2b250012e445579208e0f5d3cc06c2985
shashank-shark <shashankreddy231@gmail.com>
2019-07-06 15:24:07 +0530

    code added for demo.

---
+++ ArduinoWeb/code/arduino_code/demo_code/demo_code.ino
@@ -0,0 +1,15 @@
+int analogOutPin = 9;
+int outputValue = 0;
+void setup() {
+  pinMode(analogOutPin, OUTPUT);
+}
+
+void loop() {
+  if (outputValue >= 40){
+     outputValue = 0;
+  } else {
+     outputValue = outputValue + 1;
+  }
+  analogWrite(analogOutPin, outputValue);
+  delay(200);
+}
